63163e576f570d85dc8ba39432b3d44cb0a03570
Narala Geetavarshini <23eg105j43@anurag.edu.in>
2026-05-23 18:51:12 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -21,10 +21,15 @@ connectDB();
 
 
 
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://heath-care-management.vercel.app"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json({
   limit: "1mb"
 }));
